2cf78c824ed581f668a2c7c1ec9dd6c435880b46
lucas <lucas@lucastakushi.com>
2017-12-05 11:38:09 -0200

    Implementing greetings, contacts and polls flows

--- app/app.js
+++ app/app.js
@@ -2,12 +2,19 @@
 require('dotenv').config();
 require('./connectorSetup.js');
 
-const doido = require('./mandatoaberto_api.js');
-doido.greetings();
+const MandaAbertoAPI = require('./mandatoaberto_api.js');
 
 let DialogFlowReconizer = require('./dialogflow_recognizer.js');
 
-const request = require('request');
+// Opções de botões
+const AboutPolitician = "Quero saber";
+const Poll            = "Responder enquete";
+const Contacts        = "Contatos";
+const Yes             = "Sim";
+const No              = "Não";
+
+let politicianData;
+let pollData;
 
 let intents = new builder.IntentDialog({
     recognizers: [
@@ -21,12 +28,184 @@ intents.matches('greetings', '/greetings');
 
 bot.dialog('/', intents);
 
-// On Get_Started Facebook button
+// Ação disparada pelo botão 'get_started' do Facebook
 bot.beginDialogAction('getstarted', '/greetings');
 
 bot.dialog('/greetings', [
-    (session, args, next) => {
-        console.log(doido);
-        session.send("Oi");
+    function (session, args, next) {
+        
+        function callback(politician) {
+            politicianData = politician;
+            session.sendTyping();
+            session.send('Olá! Sou o assessor digital do ' + politicianData.office.name + ' ' + politicianData.name);
+            builder.Prompts.choice(session,
+                'Em que assunto eu posso te ajudar?',
+                [AboutPolitician, Poll],
+                {
+                    listStyle: builder.ListStyle.button,
+                    retryPrompt: 'fail'
+                }
+            );
+        }
+
+        MandaAbertoAPI.getPoliticianData(callback);
+    },
+
+    function (session, args) {
+        if (args.response) {
+            switch (args.response.entity) {
+                case AboutPolitician:
+                    session.sendTyping();
+                    session.send(politicianData.greeting.text);
+                    builder.Prompts.choice(session,
+                        'O que mais deseja saber sobre o ' + politicianData.office.name + '?',
+                        [Contacts],
+                        {
+                            listStyle: builder.ListStyle.button,
+                            retryPrompt: 'fail'
+                        }
+                    );
+                    break;
+                case Poll:
+                    session.replaceDialog('/poll');
+                    break;
+            }
+        }
+    },
+
+    function (session, args) {
+        if (args.response) {
+            switch (args.response.entity) {
+                case Contacts:
+                    session.replaceDialog('/contact');
+                    break;
+            }
+        }
+    },
+
+    function (session, args) {
+        if (args.response) {
+            switch (args.response.entity) {
+                case AboutPolitician:
+                    session.sendTyping();
+                    session.send(politicianData.greeting.text);
+                    builder.Prompts.choice(session,
+                        'O que mais deseja saber sobre o ' + politicianData.office.name + '?',
+                        [Contacts],
+                        {
+                            listStyle: builder.ListStyle.button,
+                            retryPrompt: 'fail'
+                        }
+                    );
+                    break;
+                case Poll:
+                    session.replaceDialog('/poll');
+                    break;
+            }
+        }
+    },
+]);
+
+bot.dialog('/contact', [
+    function (session, args) {
+        session.sendTyping();
+        session.send(
+            'Você pode entrar em contato com o ' + politicianData.office.name +
+            'através do e-mail: ' + politicianData.contact.email + ', pelo telefone: ' + politicianData.contact.cellphone
+            + ', e até pelo seu Twitter: ' + politicianData.contact.twitter
+        );
+        builder.Prompts.choice(session,
+            'Posso te ajudar com outra coisa?',
+            [ Contacts, Poll ],
+            {
+                listStyle: builder.ListStyle.button,
+                retryPrompt: 'fail'
+            }
+        );
+    },
+
+    function (session, args) {
+        if (args.response) {
+            switch (args.response.entity) {
+                case Contacts:
+                    session.replaceDialog('/contact');
+                    break;
+                case Poll:
+                    session.replaceDialog('/poll');
+                    break;
+            }
+        }
     }
+]);
+
+bot.dialog('/poll', [
+    function (session, args) {
+        function callback(poll) {
+            pollData = poll;
+            session.sendTyping();
+            session.send("Que legal, é muito importante entender o que a população pensa para criarmos iniciativas que de fato impactem positivamente na vida de todos.");
+            builder.Prompts.choice(session,
+                pollData.polls[0].questions[0].content,
+                [ pollData.polls[0].questions[0].options[0].content, pollData.polls[0].questions[0].options[1].content ],
+                {
+                    listStyle: builder.ListStyle.button,
+                    retryPrompt: 'fail'
+                }
+            );
+        }
+
+        MandaAbertoAPI.getPoll(callback)
+    },
+
+    function (session, args) {
+        if (args.response) {
+            switch (args.response.entity) {
+                case pollData.polls[0].questions[0].options[0].content:
+                    session.sendTyping();
+                    session.send("Muito obrigado, é muito importante a participação da população nesse processo de elaboração de projetos.");
+                    builder.Prompts.text(session, "Você gostaria de assinar a nossa petição para dar mais força ao projeto? Para isso é só me falar seu email, vamos la?");
+                    break;
+                case pollData.polls[0].questions[0].options[1].content:
+                    session.sendTyping();
+                    session.send("Muito obrigado, é muito importante a participação da população nesse processo de elaboração de projetos.");
+                    session.replaceDialog('/greetings');
+                    break;
+            }
+        }
+    },
+
+    function (session, args) {
+        if (args.response) {
+            session.dialogData.email = args.response;
+            session.sendTyping();
+            builder.Prompts.text(session, "Legal, agora pode me informar seu telefone, para lhe manter informado sobre outras enquetes?");
+        }
+    },
+
+    function (session, args) {
+        if (args.response) {
+            session.dialogData.cellphone = args.response;
+
+            session.sendTyping();
+            session.send("Pronto, já guardei seus dados. Vou lhe enviar o resultado atual da enquete, e assim que terminar a pesquisa eu lhe envio o resultado final");
+            builder.Prompts.choice(session,
+                'Posso te ajudar com outra coisa?',
+                [ AboutPolitician ],
+                {
+                    listStyle: builder.ListStyle.button,
+                    retryPrompt: 'fail'
+                }
+            );
+        }
+    },
+
+    function (session, args) {
+        if (args.response) {
+            switch (args.response.entity) {
+                case AboutPolitician:
+                    session.replaceDialog('/greetings');
+                    break;
+            }
+        }
+    },
 ]);
--- app/mandatoaberto_api.js
+++ app/mandatoaberto_api.js
@@ -12,31 +12,9 @@ var password = process.env.CHATBOT_USER_PASSWORD;
 
 let api_key;
 let user_id;
-function auth(getPoliticianData) {
-    let options = {
-        url: apiUri + 'api/login',
-        headers: headers,
-        method: 'POST',
-        form:{
-            email: email,
-            password: password
-        },
-        json: true
-    };
-
-    request(options, getPoliticianData)
-    .then((body) => {
-        api_key = body.api_key;
-        user_id = body.user_id;
-        return getPoliticianData(api_key, user_id);
-    })
-    .catch((err) => {
-        console.log(err)
-    });
-}
 
 module.exports = {
-    greetings: function() {
+    getPoliticianData: function(callback) {
 
         // Primeiro realizo a autenticação
         let options = {
@@ -63,36 +41,51 @@ module.exports = {
             request(options)
             .then((body) => {
                 let politician = body;
-                console.log(politician);
+                return callback(politician);
             })
+            .catch((err) => {
+                console.log(err)
+            });
         })
         .catch((err) => {
             console.log(err)
         });
     },
+
+    getPoll: function(callback) {
+        // Primeiro realizo a autenticação
+        let options = {
+            url: apiUri + 'api/login',
+            headers: headers,
+            method: 'POST',
+            form:{
+                email: email,
+                password: password
+            },
+            json: true
+        };
+        
+        request(options)
+        .then((body) => {
+            api_key = body.api_key;
+            user_id = body.user_id;
+
+            // Depois puxo os dados do representante público
+            options.method = 'GET'
+            options.url    = apiUri + 'api/poll';
+            options.form   = { api_key: api_key };
+
+            request(options)
+            .then((body) => {
+                let poll = body;
+                return callback(poll);
+            })
+            .catch((err) => {
+                console.log(err)
+            });
+        })
+        .catch((err) => {
+            console.log(err)
+        });
+    }
 }
-
-// module.exports = {
-//     greetings: function() {
-//         auth(getPoliticianData);
-
-//         function getPoliticianData(api_key, user_id) {
-            
-//             let options = {
-//                 url: apiUri + 'api/politician/' + user_id,
-//                 headers: headers,
-//                 method: 'GET',
-//                 form:{ api_key: api_key },
-//                 json: true
-//             };
-
-//             request(options)
-//                 .then((body) => {
-//                     console.log(body);
-//                 })
-//                 .catch((err) => {
-//                     console.log(err)
-//                 });
-//         }
-//     },
-// }
