621dfbf29758f7256d956da32394d29309fae3c0
Lucas Ansei <lucas.ansei@eokoe.com>
2017-12-18 14:02:22 -0200

    Adding mandato_aberto api connector

--- app/index.js
+++ app/index.js
@@ -2,24 +2,11 @@ require('dotenv').config();
 
 const { MessengerBot, FileSessionStore } = require('bottender');
 const { createServer } = require('bottender/restify');
-
+const MandaAbertoAPI = require('./mandatoaberto_api.js');
 const config = require('./bottender.config.js').messenger;
 
-const PAGE_1_ID = process.env.PAGE_1_ID;
-const PAGE_1_ACCESS_TOKEN = process.env.PAGE_1_ACCESS_TOKEN;
-
-const PAGE_2_ID = process.env.PAGE_2_ID;
-const PAGE_2_ACCESS_TOKEN = process.env.PAGE_2_ACCESS_TOKEN;
-
 const mapPageToAccessToken = pageId => {
-  switch (pageId) {
-	case PAGE_1_ID:
-		return PAGE_1_ACCESS_TOKEN;
-	case PAGE_2_ID:
-		return PAGE_2_ACCESS_TOKEN;
-	default:
-		return PAGE_2_ACCESS_TOKEN;
-  }
+	console.log(MandaAbertoAPI);
 };
 
 const bot = new MessengerBot({
