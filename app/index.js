adff925f274fcbf8ad66ea01bd56fe3d270831d6
lucas <lucas@lucastakushi.com>
2017-12-19 09:48:16 -0200

    adding botstate control

--- app/index.js
+++ app/index.js
@@ -31,22 +31,55 @@ const bot = new MessengerBot({
 	sessionStore: new FileSessionStore(),
 });
 
+bot.setInitialState({
+    dialog: 'greetings',
+});
+
+
 bot.onEvent(async context => {
-    console.log(context._session);
-    const introText = `Olá ${context._session.user.first_name}!, sou o assistente digital ${articles.possessive} ${politicianData.office.name} ${politicianData.name}!. Seja benvindo a nossa Rede! Queremos um Brasil a melhor e precisamos de sua ajuda.`;
-    await context.sendQuickReplies({ text: introText }, [
-        {
-          content_type: 'text',
-          title: 'Quero saber',
-          payload: 'about_me',
-        },
-        {
-          content_type: 'text',
-          title: 'Responder enquete',
-          payload: 'poll',
-        },
-    ]);
-	console.log(politicianData);
+
+    if (context.event.isQuickReply && context.state.dialog == 'prompt') {
+        const payload = context.event.message.quick_reply.payload;
+        await context.setState( { dialog: payload } );
+    }
+
+    switch (context.state.dialog) {
+        case 'greetings':
+            const introText = `Olá ${context._session.user.first_name}!, sou o assistente digital ${articles.possessive} ${politicianData.office.name} ${politicianData.name}!. Seja benvindo a nossa Rede! Queremos um Brasil a melhor e precisamos de sua ajuda.`;
+            await context.sendQuickReplies({ text: introText }, [
+                {
+                  content_type: 'text',
+                  title: 'Quero saber',
+                  payload: 'about_me',
+                },
+                {
+                  content_type: 'text',
+                  title: 'Responder enquete',
+                  payload: 'poll',
+                },
+            ]);
+
+            await context.setState( { dialog: 'prompt' } );
+
+            break;
+        case 'about_me':
+            await context.sendText(politicianData.greeting);
+
+            await context.sendQuickReplies({ text: `O que mais deseja saber sobre ${articles.defined} ${politicianData.office.name}?` }, [
+                {
+                  content_type: 'text',
+                  title: 'Quero saber',
+                  payload: 'about_me',
+                },
+                {
+                  content_type: 'text',
+                  title: 'Responder enquete',
+                  payload: 'poll',
+                },
+            ]);
+
+            break;
+    }
 });
 
 const server = createServer(bot, { verifyToken: config.verifyToken } );
