e4cbfba608728886eece5eed5e2ec289fe0950e4
lucas <lucas@lucastakushi.com>
2017-12-19 10:08:05 -0200

    Adding state set to greetings

--- app/index.js
+++ app/index.js
@@ -16,16 +16,12 @@ const mapPageToAccessToken = (async pageId => {
 	politicianData = await MandatoAbertoAPI.getPoliticianData(pageId);
 
 	// Deve-se indentificar o sexo do representante público
-    // e selecionar os artigos (definido e possesivo) adequados
-    if (politicianData.gender === 'F') {
-        articles = Articles.feminine;
-    } else {
-        articles = Articles.masculine;
-    }
-
-    await bot.setInitialState({
-        dialog: 'greetings',
-    });
+	// e selecionar os artigos (definido e possesivo) adequados
+	if (politicianData.gender === 'F') {
+		articles = Articles.feminine;
+	} else {
+		articles = Articles.masculine;
+	}
 
 	return politicianData.fb_access_token;
 });
@@ -36,51 +32,55 @@ const bot = new MessengerBot({
 	sessionStore: new FileSessionStore(),
 });
 
+bot.setInitialState({});
+
 bot.onEvent(async context => {
-    console.log(context.state);
+	if (!context.state.dialog) {
+		await context.setState( { dialog: 'greetings' } )
+	}
 
-    if (context.event.isQuickReply && context.state.dialog == 'prompt') {
-        const payload = context.event.message.quick_reply.payload;
-        await context.setState( { dialog: payload } );
-    }
+	if (context.event.isQuickReply && context.state.dialog == 'prompt') {
+		const payload = context.event.message.quick_reply.payload;
+		await context.setState( { dialog: payload } );
+	}
 
-    switch (context.state.dialog) {
-        case 'greetings':
-            const introText = `Olá ${context._session.user.first_name}!, sou o assistente digital ${articles.possessive} ${politicianData.office.name} ${politicianData.name}!. Seja benvindo a nossa Rede! Queremos um Brasil a melhor e precisamos de sua ajuda.`;
-            await context.sendQuickReplies({ text: introText }, [
-                {
-                  content_type: 'text',
-                  title: 'Quero saber',
-                  payload: 'about_me',
-                },
-                {
-                  content_type: 'text',
-                  title: 'Responder enquete',
-                  payload: 'poll',
-                },
-            ]);
+	switch (context.state.dialog) {
+		case 'greetings':
+			const introText = `Olá ${context._session.user.first_name}!, sou o assistente digital ${articles.possessive} ${politicianData.office.name} ${politicianData.name}!. Seja benvindo a nossa Rede! Queremos um Brasil a melhor e precisamos de sua ajuda.`;
+			await context.sendQuickReplies({ text: introText }, [
+				{
+					content_type: 'text',
+					title: 'Quero saber',
+					payload: 'about_me',
+				},
+				{
+					content_type: 'text',
+					title: 'Responder enquete',
+					payload: 'poll',
+				},
+			]);
 
-            await context.setState( { dialog: 'prompt' } );
+			await context.setState( { dialog: 'prompt' } );
 
-            break;
-        case 'about_me':
-            await context.sendText(politicianData.greeting);
+			break;
+		case 'about_me':
+			await context.sendText(politicianData.greeting);
 
-            await context.sendQuickReplies({ text: `O que mais deseja saber sobre ${articles.defined} ${politicianData.office.name}?` }, [
-                {
-                  content_type: 'text',
-                  title: 'Quero saber',
-                  payload: 'about_me',
-                },
-                {
-                  content_type: 'text',
-                  title: 'Responder enquete',
-                  payload: 'poll',
-                },
-            ]);
+			await context.sendQuickReplies({ text: `O que mais deseja saber sobre ${articles.defined} ${politicianData.office.name}?` }, [
+				{
+					content_type: 'text',
+					title: 'Quero saber',
+					payload: 'about_me',
+				},
+				{
+					content_type: 'text',
+					title: 'Responder enquete',
+					payload: 'poll',
+				},
+			]);
 
-            break;
-    }
+			break;
+	}
 });
 
 const server = createServer(bot, { verifyToken: config.verifyToken } );
