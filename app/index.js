ba5ab7be1f4bceffdd06290fb65aaa8507eb0f3a
Jordan Victor <jordanvscher@hotmail.com>
2018-05-02 17:04:30 -0300

    The end(?)

--- app/index.js
+++ app/index.js
@@ -18,9 +18,9 @@ function getMoney(str) {
 function formatReal(int) {
         var tmp = int+'';
         tmp = tmp.replace(/([0-9]{2})$/g, ",$1");
-        if( tmp.length > 6 )
+        if( tmp.length > 6 ) {
                 tmp = tmp.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
-
+							}
         return tmp;
 }
 
