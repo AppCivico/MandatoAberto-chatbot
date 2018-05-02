6eb0d8d1e7827bd8971766d62628827da6463b1b
Jordan Victor <jordanvscher@hotmail.com>
2018-05-02 15:25:57 -0300

    Logging env

--- app/votolegal_api.js
+++ app/votolegal_api.js
@@ -1,10 +1,10 @@
 const request = require('requisition');
 
 const VotoLegalapiUri = process.env.VOTO_LEGAL_API_URL;
+console.log(`${VotoLegalapiUri}/api/candidate/${username}`);
 
 module.exports = {
 	async getVotoLegalValues(username) {
-		// ?id=josehernandes#/
 		console.log(`${VotoLegalapiUri}/api/candidate/${username}`);
 		const res = await request(`${VotoLegalapiUri}/api/candidate/${username}`);
 		const votolegal = await res.json();
