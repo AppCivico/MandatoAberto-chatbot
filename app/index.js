f0f654ba95893af6835af41fb4b357d98b54c137
Jordan Victor <jordanvscher@hotmail.com>
2018-05-02 15:53:21 -0300

    Correct division

--- app/index.js
+++ app/index.js
@@ -396,7 +396,7 @@ bot.onEvent(async (context) => {
 		];
 		const valueLegal = await VotoLegalAPI.getVotoLegalValues(politicianData.votolegal_integration.votolegal_username);
 		await context.sendText(`Estou fazendo uma campanha para minha pré-campanha, e minha meta é de R$${valueLegal.candidate.raising_goal}.`);
-		await context.sendText(`Já consegui R$ ${valueLegal.candidate.total_donated % 100}. Seria muito importante sua colaboração!`);
+		await context.sendText(`Já consegui R$ ${valueLegal.candidate.total_donated / 100}. Seria muito importante sua colaboração!`);
 		await context.sendButtonTemplate('Utilizamos a plataforma VotoLegal para realizar as doações:', postbackOptions);
 		await context.setState({ dialog: 'prompt' });
 	break;
