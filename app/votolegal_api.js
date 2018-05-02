3325ae3607e64d85990f412daf37163f7c2d1a96
Jordan Victor <jordanvscher@hotmail.com>
2018-05-02 15:07:26 -0300

    api/candidate/

--- app/votolegal_api.js
+++ app/votolegal_api.js
@@ -4,7 +4,7 @@ const VotoLegalapiUri = process.env.VOTO_LEGAL_API_URL;
 
 module.exports = {
 	async getVotoLegalValues(username) {
-		const res = await request(`${VotoLegalapiUri}/api/chatbot/${username}`);
+		const res = await request(`${VotoLegalapiUri}/api/candidate/${username}`);
 		const votolegal = await res.json();
 		return votolegal;
 	},
