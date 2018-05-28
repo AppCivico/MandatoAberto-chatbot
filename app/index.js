6cbeb2b00364ce25c8bb6664353c0a2c62e2fb70
Jordan Victor <jordanvscher@hotmail.com>
2018-05-28 15:31:34 -0300

    Added a verification

--- app/index.js
+++ app/index.js
@@ -886,14 +886,12 @@ bot.onEvent(async context => {
 
       break;
     case "recipientData":
-      if (
-        context.event.postback.title == "Agora não" ||
-        context.event.postback.title == "Não"
-      ) {
+      if (context.event.postback) {
+    if (context.event.postback.title == "Agora não" || context.event.postback.title == "Não") {
         await context.sendButtonTemplate("Está bem! Posso te ajudar com mais alguma informação?", promptOptions);
 
         await context.setState({ dialog: "prompt" });
-      } else if (context.state.dataPrompt) {
+      } } else if (context.state.dataPrompt) {
         switch (context.state.dataPrompt) {
           case "email":
             await context.sendText("Qual é o seu e-mail?");
