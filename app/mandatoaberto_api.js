6b02bd1b7faf180e57199e6e0d3ea1bf9c298a02
Jordan Victor <jordanvscher@hotmail.com>
2018-05-29 12:14:53 -0300

    log privateReply

--- app/mandatoaberto_api.js
+++ app/mandatoaberto_api.js
@@ -58,6 +58,7 @@ module.exports = {
 	async postPrivateReply(item, page_id, post_id, comment_id, permalink) {
 		const res = await request.post(`${apiUri}/api/chatbot/private-reply?page_id=${page_id}&item=${item}&post_id=${post_id}&comment_id=${comment_id}&permalink=${permalink}&security_token=${security_token}`);
 		const privateReply = await res.json();
+		console.log('privateReply', privateReply);
 		return privateReply;
 	}
 };
