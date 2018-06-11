df3281ae96a9420ab4294cbdaace8979d2ecbdf1
Jordan Victor <jordanvscher@hotmail.com>
2018-06-11 15:25:52 -0300

    pollanswer

--- app/index.js
+++ app/index.js
@@ -261,13 +261,14 @@ bot.onEvent(async context => {
     await context.typingOff();
     const payload = context.event.postback.payload;
     if (payload === "mainMenu") {
+      await context.setState({ dataMessage: "Muito obrigado por sua mensagem. Você gostaria de deixar ou atualizar seu e-mail e telefone para nossa equipe?" });
       await MandatoAbertoAPI.postIssue(
         politicianData.user_id,
         context.session.user.id,
         context.state.userMessage
       );
       await context.setState({ userMessage: ''});
-
+      await context.setState({ dialog: pollAnswer});
     } else if (context.event.message) {
         context.event.message.text = "";
     }
@@ -840,6 +841,7 @@ bot.onEvent(async context => {
             }
           ]
         );
+        await context.setState({ dataMessage: "Muito obrigado por sua resposta.Você gostaria de deixar seu e-mail e telefone para nossa equipe?" });
         await context.setState({ dialog: "pollAnswer" });
       }
       break;
