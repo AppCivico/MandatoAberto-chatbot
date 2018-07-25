0554e30c38936f391ec32f3ae93666ca6abd4461
Jordan Victor <jordanvscher@hotmail.com>
2018-07-25 13:58:10 -0300

    Changed votoLegal verification

--- app/index.js
+++ app/index.js
@@ -67,10 +67,8 @@ function checkMenu(context, opt2) { // eslint-disable-line no-inner-declarations
   if (!context.state.introduction) { dialogs = dialogs.filter(obj => obj.payload !== 'aboutPolitician'); }
   if (!context.state.trajectory) { dialogs = dialogs.filter(obj => obj.payload !== 'trajectory');}
   if (!context.state.pollData) { dialogs = dialogs.filter(obj => obj.payload !== 'poll'); }
-  if (!context.state.politicianData.contact) { dialogs = dialogs.filter(obj => obj.payload !== 'contacts'); console.log('Here i am')}
-  if (!context.state.politicianData.votolegal_integration2.votolegal_username)
-  { dialogs = dialogs.filter(obj => obj.payload !== 'votoLegal'); }
-  // if (!context.state.politicianData.votolegal_integration) { dialogs = dialogs.filter(obj => obj.payload !== 'votoLegal'); }
+  if (!context.state.politicianData.contact) { dialogs = dialogs.filter(obj => obj.payload !== 'contacts');}
+  if (!context.state.politicianData.votolegal_integration) { dialogs = dialogs.filter(obj => obj.payload !== 'votoLegal'); console.log('Here i am')}
   console.log(dialogs);
   return dialogs;
 }
