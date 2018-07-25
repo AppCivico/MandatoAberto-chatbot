3d476d5b0f6a80a4b35cb5fe68307c228dc28aea
Jordan Victor <jordanvscher@hotmail.com>
2018-07-25 16:29:26 -0300

    Test with fixed index

--- app/index.js
+++ app/index.js
@@ -82,7 +82,7 @@ async function checkMenu(context, opt2) { // eslint-disable-line no-inner-declar
   if (!context.state.politicianData.contact) { dialogs = dialogs.filter(obj => obj.payload !== 'contacts');}
   if (!context.state.politicianData.votolegal_integration) { dialogs = dialogs.filter(obj => obj.payload !== 'votoLegal');}
   // dialogs.find(obj => obj.payload === 'aboutPolitician', (obj) => (obj.title = getAboutMe(context.state.politicianData)));
-  dialogs.find(obj => obj.payload === 'aboutPolitician', () => (console.log('sdfsdf')));
+  if (dialogs[0].payload === 'aboutPolitician') { dialogs[0].title = getAboutMe(context.state.politicianData)}
   console.log(dialogs);
   return dialogs;
 }
