fdd8fcfe01514b9c1639462bcba39b9dab2a7807
Jordan Victor <jordanvscher@hotmail.com>
2018-07-25 15:09:53 -0300

    removed await

--- app/index.js
+++ app/index.js
@@ -81,7 +81,7 @@ function checkMenu(context, opt2) { // eslint-disable-line no-inner-declarations
   if (!context.state.pollData) { dialogs = dialogs.filter(obj => obj.payload !== 'poll'); }
   if (!context.state.politicianData.contact) { dialogs = dialogs.filter(obj => obj.payload !== 'contacts');}
   if (!context.state.politicianData.votolegal_integration) { dialogs = dialogs.filter(obj => obj.payload !== 'votoLegal');}
-  if (dialogs.aboutPolitician) { dialogs.aboutPolitician.title = await getAboutMe(context.state.politicianData); }
+  if (dialogs.aboutPolitician) { dialogs.aboutPolitician.title = getAboutMe(context.state.politicianData); }
   console.log(dialogs);
   return dialogs;
 }
