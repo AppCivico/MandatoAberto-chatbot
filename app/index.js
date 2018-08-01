92821fc9a3b3ae7c58b13a2f355806c980cedee9
JordanVScher <jordanvscher@hotmail.com>
2018-08-01 16:13:53 -0300

    New Test

--- app/index.js
+++ app/index.js
@@ -438,7 +438,6 @@ const handler = new MessengerHandler()
 				await context.setState({ dialog: 'prompt' });
 				break;
 			}
-
 			case 'contacts':
 			// Tratando o formato do telefone
 				if (context.state.politicianData.contact.cellphone) {
