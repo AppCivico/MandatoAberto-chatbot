49abdb3f9cf304d5fa87bcd0982c3a7332eea8e7
JordanVScher <jordanvscher@hotmail.com>
2018-08-10 18:47:27 -0300

    Better time

--- app/index.js
+++ app/index.js
@@ -33,8 +33,8 @@ function formatReal(int) {
 	return tmp;
 }
 
-const IssueTimerlimit = 10000 * 2; // 20 seconds
-// const IssueTimerlimit = 10000 * 0.2; // 2 seconds
+// const IssueTimerlimit = 10000 * 2; // 20 seconds
+const IssueTimerlimit = 10000 * 0.2; // 2 seconds
 
 let timer;
 // userMessage -> context.state.userMessage -> stores the texts the user wirtes before sending them to politician [issue]
