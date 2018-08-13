9d76bb8cdf249f6862a978110d4dabc241d307af
JordanVScher <jordanvscher@hotmail.com>
2018-08-13 13:46:03 -0300

    reload test

--- app/index.js
+++ app/index.js
@@ -177,35 +177,37 @@ const handler = new MessengerHandler()
 			// Tratando dinâmica de issues
 			if (context.state.dialog === 'prompt') {
 				if (context.event.isPostback) {
+					console.log('entrei aqui');
+
 					const { payload } = context.event.postback;
 					if (payload.slice(0, 6) === 'answer') {
+						console.log('entrei aqui2');
+
 						await context.setState({ question: context.state.knowledge.knowledge_base.find(x => x.id === parseInt(payload.replace('answer', ''), 10)) });
 						await context.setState({ dialog: 'showAnswer' });
 					} else {
 						await context.setState({ dialog: payload });
 					}
 				} else if (context.event.isQuickReply) {
-					let { payload } = context.event.message.quick_reply;
+					const { payload } = context.event.message.quick_reply;
 					if (payload.slice(0, 6) === 'option') {
-						payload = payload.replace('option', '');
-						await context.setState({
-							knowledge: await MandatoAbertoAPI.getknowledgeBase(context.state.politicianData.user_id,
-								{ [payload]: context.state.apiaiResp.result.parameters[payload] }),
-						});
+						await context.setState({ payload });
+						await context.setState({ dialog: 'reload' });
 
-						await context.typingOn();
-						await attach.sendQuestions(context, context.state.knowledge.knowledge_base);
-						await context.sendText('Ok! Por favor, escolha sua pergunta acima ⤴️\nSe não achou é só clicar abaixo ⤵️', {
-							quick_replies: [
-								{
-									content_type: 'text',
-									title: 'Não achei',
-									payload: 'NotOneOfThese',
-								},
-							],
-						});
-						await context.typingOff();
-						await context.setState({ dialog: 'prompt' });
+
+						// await context.typingOn();
+						// await attach.sendQuestions(context, context.state.knowledge.knowledge_base);
+						// await context.sendText('Ok! Por favor, escolha sua pergunta acima ⤴️\nSe não achou é só clicar abaixo ⤵️', {
+						// 	quick_replies: [
+						// 		{
+						// 			content_type: 'text',
+						// 			title: 'Não achei',
+						// 			payload: 'NotOneOfThese',
+						// 		},
+						// 	],
+						// });
+						// await context.typingOff();
+						// await context.setState({ dialog: 'prompt' });
 					} else {
 						await context.setState({ dialog: payload });
 					}
@@ -358,6 +360,13 @@ const handler = new MessengerHandler()
 				await context.sendButtonTemplate(context.state.issueMessage, await checkMenu(context, [opt.aboutPolitician, opt.poll_suaOpiniao, opt.doarOption]));
 				await context.setState({ dialog: 'prompt' });
 				break;
+			case 'reload':
+				await context.setState({ payload: context.state.payload.replace('option', '') });
+				await context.setState({
+					knowledge: await MandatoAbertoAPI.getknowledgeBase(context.state.politicianData.user_id,
+						{ [context.state.payload]: context.state.apiaiResp.result.parameters[context.state.payload] }),
+				});
+			// falls through
 			case 'chooseQuestion':
 				console.log('chegou');
 
