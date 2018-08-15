cf38b7dde37844d85c14c6b6be65b071987456df
JordanVScher <jordanvscher@hotmail.com>
2018-08-15 11:24:53 -0300

    Test without subtitle

--- app/attach.js
+++ app/attach.js
@@ -5,7 +5,7 @@ async function sendQuestions(context, content) {
 	content.forEach((element, index) => {
 		elements.push({
 			title: `Pergunta #${index + 1}`,
-			subtitle: element.question,
+			// subtitle: element.question,
 			// image_url: 'http://pngimg.com/uploads/question_mark/question_mark_PNG121.png',
 			buttons: [{
 				type: 'postback',
