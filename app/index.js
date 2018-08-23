79c601439be6b69b775db39ad2d1d0770c671a89
JordanVScher <jordanvscher@hotmail.com>
2018-08-23 18:35:52 -0300

    Removed emptyKeys

--- app/index.js
+++ app/index.js
@@ -199,9 +199,8 @@ const handler = new MessengerHandler()
 					await context.setState({ apiaiResp: await apiai.textRequest(context.state.whatWasTyped, { sessionId: context.session.user.id }) });
 					console.log(context.state.apiaiResp);
 
-					removeEmptyKeys(context.state.apiaiResp.result.parameters);
+					// removeEmptyKeys(context.state.apiaiResp.result.parameters);
 
-					console.log(context.state.apiaiResp);
 
 					if (context.state.apiaiResp.result.metadata.intentName === 'Fallback') {
 						// Fallback --> counldn't find any matching intents
