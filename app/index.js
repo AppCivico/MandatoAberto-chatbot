11303c2192289b0af1f4d1b01fc874c05848daed
JordanVScher <jordanvscher@hotmail.com>
2018-08-24 11:09:09 -0300

    Testing without removeEmptyKeys

--- app/index.js
+++ app/index.js
@@ -197,7 +197,7 @@ const handler = new MessengerHandler()
 				} else if (context.event.isText) {
 					await context.setState({ whatWasTyped: context.event.message.text }); // will be used in case the bot doesn't find the question
 					await context.setState({ apiaiResp: await apiai.textRequest(context.state.whatWasTyped, { sessionId: context.session.user.id }) });
-					removeEmptyKeys(context.state.apiaiResp.result.parameters);
+					// removeEmptyKeys(context.state.apiaiResp.result.parameters);
 
 					console.log(context.state.apiaiResp.result.parameters);
 
