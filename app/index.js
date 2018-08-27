1fadf2fca6d8db6fe09747585670f7d77c4c195e
JordanVScher <jordanvscher@hotmail.com>
2018-08-27 13:47:18 -0300

    Test

--- app/index.js
+++ app/index.js
@@ -229,7 +229,7 @@ const handler = new MessengerHandler()
 					await context.setState({ apiaiResp: await apiai.textRequest(context.state.whatWasTyped, { sessionId: context.session.user.id }) });
 					removeEmptyKeys(context.state.apiaiResp.result.parameters);
 
-					console.log(context.state.apiaiResp.result.parameters);
+					// console.log(context.state.apiaiResp.result.parameters);
 
 					if (context.state.apiaiResp.result.metadata.intentName === 'Fallback') {
 						// Fallback --> counldn't find any matching intents
