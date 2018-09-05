6cad57e2d7f067f389eedab0e8dee5a03308e2e5
JordanVScher <jordanvscher@hotmail.com>
2018-09-05 17:57:15 -0300

    Test

--- app/index.js
+++ app/index.js
@@ -150,7 +150,7 @@ const handler = new MessengerHandler()
 			await context.setState({ pollData: await MandatoAbertoAPI.getPollData(context.event.rawEvent.recipient.id) });
 
 			console.log(context.state.politicianData.picframe_url);
-			
+
 			if (context.state.dialog !== 'recipientData' && context.state.dialog !== 'pollAnswer') { // handling input that's not from "asking data" or answering poll (obs: 'pollAnswer' from timer will bypass this)
 				if (context.event.isPostback) {
 					// we are not listening anymore if user clicks on persistent menu during the listening
