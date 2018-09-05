9513bc6e11af95b5c5212ba6b9c897e1140c808b
JordanVScher <jordanvscher@hotmail.com>
2018-09-05 19:04:25 -0300

    Test

--- app/index.js
+++ app/index.js
@@ -14,6 +14,8 @@ const opt = require('./utils/options');
 const dictionary = require('./utils/dictionary');
 const audio = require('./utils/audio');
 
+console.log(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+
 const apiai = dialogFlow(process.env.DIALOGFLOW_TOKEN);
 
 const phoneRegex = new RegExp(/^\+55\d{2}(\d{1})?\d{8}$/);
