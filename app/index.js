a6b46b33d419ffc1584b1dc80bc178ab09611c69
JordanVScher <jordanvscher@hotmail.com>
2018-09-10 17:17:52 -0300

    Test

--- app/index.js
+++ app/index.js
@@ -474,7 +474,7 @@ const handler = new MessengerHandler()
 				await context.setState({ whatWasTyped: '', dialog: 'prompt' });
 				break;
 			case 'audio':
-				await audio.voiceRequest(context, 'https://cdn.fbsbx.com/v/t59.3654-21/41422332_1965526987077956_6964334129533943808_n.mp4/audioclip-1536591135000-2694.mp4?_nc_cat=0&oh=4eed936c79d2011ca51995370fe1b718&oe=5B998567', context.session.user.id, (result) => { testeAudio(context, result); });
+				await audio.voiceRequest('https://cdn.fbsbx.com/v/t59.3654-21/41422332_1965526987077956_6964334129533943808_n.mp4/audioclip-1536591135000-2694.mp4?_nc_cat=0&oh=4eed936c79d2011ca51995370fe1b718&oe=5B998567', context.session.user.id, (result) => { testeAudio(context, result); });
 				break;
 			case 'checkPosition':
 				await context.setState({ dialog: 'prompt' });
