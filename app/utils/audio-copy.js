0c3b62a11d411e87db4afd52a9aa17f26bb099a6
JordanVScher <jordanvscher@hotmail.com>
2018-09-10 15:55:45 -0300

    Test

--- app/utils/audio-copy.js
+++ app/utils/audio-copy.js
@@ -13,12 +13,6 @@ const fse = require('fs-extra');
 const util = require('util');
 require('util.promisify').shim();
 
-async function returnResult(result) {
-	console.log('result', result);
-
-	return result;
-}
-
 const projectId = process.env.PROJECT_ID;
 
 // Ops: dialogflow needs a GOOGLE_APPLICATION_CREDENTIALS env with the path to the json key
@@ -59,83 +53,70 @@ async function voiceRequest(urlMessenger, sessionID, testeAudio) {
 	const answer = await request(urlMessenger);
 	await answer.pipe(file);
 
-	file.on('finish', async () => {
+	const result3 = await file.on('finish', async () => {
 		// converting the mp4 file to a mono channel flac (we have to convert before checking for duration because of 'moov atom' issues)
 		const results = await execAsync(`ffmpeg -i ${fileIn} -ac 1 -movflags +faststart ${fileOut} -y`);
-		console.log(results);
+		if (results.error && results.error.code) {
+			await checkAndDelete(fileIn);
+			await checkAndDelete(fileOut);
+			console.log('Error at conversion => ', results.error);
+			return { textMsg: 'Não entendi o que você disse. Por favor, tente novamente.' };
+		} // no error
+		// checking flac duration, it can't be bigger than 60s
+		const result2 = await getDuration(fileOut).then(async (duration) => {
+			console.log(duration);
 
-		// const dir = await exec(`ffmpeg -i ${fileIn} -ac 1 -movflags +faststart ${fileOut} -y`, async (err) => {
-		// 	if (err) {
-		// 		await checkAndDelete(fileIn);
-		// 		await checkAndDelete(fileOut);
-		// 		console.log('Error at conversion => ', err);
-		// 	}
-		// });
+			if (duration < 60) {
+				// Read the content of the audio file and send it as part of the request
+				const readFile = await util.promisify(fs.readFile, { singular: true });
+				const result = await readFile(`${fileOut}`)
+					.then((inputAudio) => {
+						// The audio query request
+						const requestOptions = {
+							session: sessionPath,
+							queryInput,
+							inputAudio,
+						};
+						// Recognizes the speech in the audio and detects its intent
+						return sessionClient.detectIntent(requestOptions);
+					}).then(async (responses) => {
+						// console.log('Detected intent => ', responses);
 
-		// dir.on('exit', async (code) => { // eslint-disable-line
-		// 	if (code === 0) { // mp4 converted to flac successfully
-		// 		// checking flac duration, it can't be bigger than 60s
-		// 		const result2 = await getDuration(fileOut).then(async (duration) => {
-		// 			console.log(duration);
+						await checkAndDelete(fileIn);
+						await checkAndDelete(fileOut);
 
-		// 			if (duration < 60) {
-		// 				// Read the content of the audio file and send it as part of the request
-		// 				const readFile = await util.promisify(fs.readFile, { singular: true });
-		// 				const result = await readFile(`${fileOut}`)
-		// 					.then((inputAudio) => {
-		// 						// The audio query request
-		// 						const requestOptions = {
-		// 							session: sessionPath,
-		// 							queryInput,
-		// 							inputAudio,
-		// 						};
-		// 						// Recognizes the speech in the audio and detects its intent
-		// 						return sessionClient.detectIntent(requestOptions);
-		// 					}).then(async (responses) => {
-		// 						// console.log('Detected intent => ', responses);
+						const detected = responses[0].queryResult;
+						if (detected && detected.queryText !== '') { // if there's no text we simlpy didn't get what the user said
+							// format parameters the same way dialogFlow does with text
+							const detectedParameters = {};
+							for (const element of Object.keys(detected.parameters.fields)) { // eslint-disable-line no-restricted-syntax
+								// removes empty parameters
+								if (detected.parameters.fields[element].listValue && detected.parameters.fields[element].listValue.values.length !== 0) {
+									// get multiple words that are attached to one single entity
+									detectedParameters[element] = detected.parameters.fields[element].listValue.values.map(obj => obj.stringValue);
+								}
+							}
 
-		// 						await checkAndDelete(fileIn);
-		// 						await checkAndDelete(fileOut);
-
-		// 						const detected = responses[0].queryResult;
-		// 						if (detected && detected.queryText !== '') { // if there's no text we simlpy didn't get what the user said
-		// 							// format parameters the same way dialogFlow does with text
-		// 							const detectedParameters = {};
-		// 							for (const element of Object.keys(detected.parameters.fields)) { // eslint-disable-line no-restricted-syntax
-		// 								// removes empty parameters
-		// 								if (detected.parameters.fields[element].listValue && detected.parameters.fields[element].listValue.values.length !== 0) {
-		// 									// get multiple words that are attached to one single entity
-		// 									detectedParameters[element] = detected.parameters.fields[element].listValue.values.map(obj => obj.stringValue);
-		// 								}
-		// 							}
-
-		// 							return {
-		// 								intentName: detected.intent.displayName, whatWasSaid: `[Áudio] ${detected.queryText}`, parameters: detectedParameters,
-		// 							};
-		// 						} // no text, user didn't say anything/no speech was detected
-		// 						return { textMsg: 'Não consegui ouvir o que você disse. Por favor, tente novamente.' };
-		// 					}).catch(async (err) => {
-		// 						console.error('ERROR:', err);
-		// 						await checkAndDelete(fileIn);
-		// 						await checkAndDelete(fileOut);
-		// 						return { textMsg: 'Não entendi o que você disse. Por favor, tente novamente.' };
-		// 					});
-		// 				return result;
-		// 			} // audio has 60+ seconds
-		// 			await checkAndDelete(fileIn);
-		// 			await checkAndDelete(fileOut);
-		// 			return { textMsg: 'Áudio muito longo! Por favor, mande áudio com menos de 1 minuto!' };
-		// 		});
-		// 		returnResult(result2);
-		// 		// return result2;
-		// 	} else { // code not 0
-		// 		console.log('Não foi possível converter os arquivos');
-		// 		await checkAndDelete(fileIn);
-		// 		await checkAndDelete(fileOut);
-		// 		return { textMsg: 'Não entendi o que você disse. Por favor, tente novamente.' };
-		// 	}
-		// }); // dir.onExit
+							return {
+								intentName: detected.intent.displayName, whatWasSaid: `[Áudio] ${detected.queryText}`, parameters: detectedParameters,
+							};
+						} // no text, user didn't say anything/no speech was detected
+						return { textMsg: 'Não consegui ouvir o que você disse. Por favor, tente novamente.' };
+					}).catch(async (err) => {
+						console.error('ERROR:', err);
+						await checkAndDelete(fileIn);
+						await checkAndDelete(fileOut);
+						return { textMsg: 'Não entendi o que você disse. Por favor, tente novamente.' };
+					});
+				return result;
+			} // audio has 60+ seconds
+			await checkAndDelete(fileIn);
+			await checkAndDelete(fileOut);
+			return { textMsg: 'Áudio muito longo! Por favor, mande áudio com menos de 1 minuto!' };
+		});
+		return result2;
 	}); // file.on('finish')
+	console.log('result3', result3);
 }
 
 
