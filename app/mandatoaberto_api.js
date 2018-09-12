033b98d24b3688a6c25cd3758a4202adbe369654
JordanVScher <jordanvscher@hotmail.com>
2018-09-12 17:04:40 -0300

    Test

--- app/mandatoaberto_api.js
+++ app/mandatoaberto_api.js
@@ -21,7 +21,7 @@ module.exports = {
 
 	async postRecipient(user_id, recipient) {
 		console.log('recipient', recipient);
-		const recipientData_qs = queryString.stringify(recipient);
+		const recipientData_qs = recipient;
 		const res = await request.post(`${apiUri}/api/chatbot/recipient?${recipientData_qs}&security_token=${security_token}&`).query({ politician_id: user_id });
 		const recipientData = await res.json();
 		console.log('recipientData', recipientData);
