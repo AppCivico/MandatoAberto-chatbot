bd7dce104e67b5538bf4a141e6a46b433a623eaa
JordanVScher <jordanvscher@hotmail.com>
2018-09-19 17:40:33 -0300

    Logging on talkTo Us

--- app/index.js
+++ app/index.js
@@ -630,8 +630,8 @@ const handler = new MessengerHandler()
 				// create new (or reset) timer for sending message
 				issueTimers[context.session.user.id] = setTimeout(async () => {
 					if (userMessages[context.session.user.id] !== '') { // check if there's a message to send
-						await MandatoAbertoAPI.postIssue(context.state.politicianData.user_id, context.session.user.id, userMessages[context.session.user.id],
-							context.state.resultParameters);
+						console.log(await MandatoAbertoAPI.postIssue(context.state.politicianData.user_id, context.session.user.id, userMessages[context.session.user.id],
+							context.state.resultParameters));
 						// console.log('Enviei ', userMessages[context.session.user.id]);
 						await context.typingOff();
 					}
