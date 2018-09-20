e3fd01b7a752fa1249c3b18aff8836f35e621b79
JordanVScher <jordanvscher@hotmail.com>
2018-09-20 14:22:51 -0300

    Test

--- app/index.js
+++ app/index.js
@@ -307,9 +307,7 @@ const handler = new MessengerHandler()
 					if (listening[context.session.user.id]) { delete listening[context.session.user.id]; }
 					// user confirms that theme(s) is/are correct
 					if (context.event.postback.payload.slice(0, 8) === 'themeYes') {
-						console.log('Payload', context.event.postback.payload);
-
-						const aaa = context.event.postback.payload.slice(9);
+						const aaa = context.event.postback.payload.replace('themeYes', '');
 						console.log('before number', aaa);
 
 						const number = context.event.postback.payload.slice(9) ? context.event.postback.payload.slice(9) : 0;
