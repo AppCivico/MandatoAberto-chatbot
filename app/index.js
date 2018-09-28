7f6c1d7db695e5908ccff824a477842a00d53187
JordanVScher <jordanvscher@hotmail.com>
2018-09-28 15:00:07 -0300

    Test

--- app/index.js
+++ app/index.js
@@ -172,7 +172,7 @@ async function checkMenu(context, dialogs) { // eslint-disable-line no-inner-dec
 async function showThemesQR(context) {
 	// TODO cachear um página pra sempre saber se tem mais intents por vir
 
-	if (!context.state.nextIntents || context.state.nextIntents.length < 10) {
+	if (!context.state.nextIntents || context.state.nextIntents === '' || context.state.nextIntents.length < 10) {
 		await context.setState({ availableIntents: await MandatoAbertoAPI.getAvailableIntents(context.event.rawEvent.recipient.id, context.state.paginationNumber) });
 		await context.setState({ nextIntents: await MandatoAbertoAPI.getAvailableIntents(context.event.rawEvent.recipient.id, context.state.paginationNumber + 1) });
 	} else {
@@ -399,7 +399,7 @@ const handler = new MessengerHandler()
 						await context.setState({ dialog: 'createIssue' });
 					} else {
 						if (context.event.postback.payload === 'availableIntents') {
-							await context.setState({ paginationNumber: 1 }); // resetting pagination
+							await context.setState({ paginationNumber: 1, availableIntents: '', nextIntents: '' }); // resetting data
 							await showThemesQR(context);
 						}
 						await context.setState({ dialog: context.event.postback.payload });
