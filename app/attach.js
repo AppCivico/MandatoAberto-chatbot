efe6716feec34e7a310426d61718be17769f79ce
JordanVScher <jordanvscher@hotmail.com>
2018-09-28 14:33:29 -0300

    Test

--- app/attach.js
+++ app/attach.js
@@ -73,7 +73,8 @@ module.exports.getIntentQR = getIntentQR;
 
 // get every label
 async function sendButtons(id, text, buttons, accessToken) { // eslint-disable-line no-unused-vars
-	const res = await req.post(`https://graph.facebook.com/v2.6/me/messages?access_token=${accessToken}`).json({
+	const res = await req.post(`https://graph.facebook.com/v2.6/me/messages?access_token=${accessToken}`).query({
+		'Content-Type': 'application/json',
 		recipient: {
 			id,
 		},
