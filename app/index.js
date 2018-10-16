b39a2c8d63ef929daa2960b9c768ecf9a4960fa1
JordanVScher <jordanvscher@hotmail.com>
2018-10-16 12:46:13 -0300

    Test

--- app/index.js
+++ app/index.js
@@ -343,7 +343,7 @@ async function checkPosition(context) {
 
 const handler = new MessengerHandler()
 	.onEvent(async (context) => { // eslint-disable-line
-		if (!context.session && context.event.isText) {
+		if (!context.state && context.event.isText) {
 			await context.setState({ dialog: 'greetings' });
 		} else if (!context.event.isDelivery && !context.event.isEcho && !context.event.isRead && context.event.rawEvent.field !== 'feed') {
 			await context.typingOn();
