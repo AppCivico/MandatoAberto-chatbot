f17fd8104d9eb49e4cd49145d41c76e23d1864e1
JordanVScher <jordanvscher@hotmail.com>
2018-10-18 11:30:10 -0300

    Logging

--- app/index.js
+++ app/index.js
@@ -780,6 +780,10 @@ const handler = new MessengerHandler()
 			case 'NotOneOfThese': // user said "no" on theme confirmation
 				if (menuTimers[context.session.user.id]) { delete menuTimers[context.session.user.id]; } // for safety reasons
 				// maybe we don't need to verify if this should be an issue because dialogflow already indentified something
+				// TODO issue_active
+				console.log(context.state.politicianData.issue_active);
+				console.log(typeof context.state.politicianData.issue_active);
+
 				if (await MandatoAbertoAPI.postIssue(context.state.politicianData.user_id, context.session.user.id,
 					context.state.whatWasTyped, context.state.apiaiResp, context.state.politicianData.issue_active) !== false) {
 					await context.sendText('Que pena! Parece que eu errei. Mas recebi sua dúvida e estaremos te respondendo logo mais! Quer fazer outra pergunta?');
